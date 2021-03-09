50dacd47815bcc9c80692297324d2f21ebee4bca
zainmstf <zainimustofa24@gmail.com>
2021-03-09 18:56:26 +0700

    Ajax untuk signup

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect('127.0.0.1', 'root', '', 'chat'); //Membuat koneksi
+if ($conn) {
+} else {
+    echo "error";
+}
---
+++ php/signup.php
@@ -0,0 +1,63 @@
+<?php
+
+session_start();
+include_once 'config.php';
+$fname = mysqli_real_escape_string($conn, $_POST['fname']);
+$lname = mysqli_real_escape_string($conn, $_POST['lname']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
+    // Cek validasi email
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}'");
+        if (mysqli_num_rows($sql) > 0) { // jika email sudah terdaftar
+            echo "$email - This email already exist";
+        } else {
+            // Cek user upload image atau tidak
+            if (isset($_FILES['image'])) {
+                $img_name = $_FILES['image']['name'];
+                $img_type = $_FILES['image']['type'];
+                $tmp_name = $_FILES['image']['tmp_name'];
+
+                $img_explode = explode('.', $img_name); // Memecah nama image
+                $img_ext = end($img_explode); // Mendapatkan extension dari user upload file
+
+                $extensions = ['png', 'jpeg', 'jpg'];
+                if (in_array($img_ext, $extensions) === true) {
+                    $time = time(); // Mengembalikan waktu saat ini, digunakan untuk menamai folder file user dengan waktu saat ini
+                    $new_img_name = $time . $img_name;
+
+                    if (move_uploaded_file($tmp_name, "images/" . $new_img_name)) {
+                        $status = "Active Now"; // setiap user mendaftar status akan me njadi aktif
+                        $random_id = rand(time(), 10000000);
+
+                        //Insert data kedalam table
+                        $sql2 = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, image, status) VALUES ({$random_id}, '{$fname}', '{$lname}', '{$email}', '{$password}', '{$new_img_name}', '{$status}')");
+                        //Jika data sudah ada
+                        if ($sql2) {
+                            $sql3 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                            if (mysqli_num_rows($sql3) > 0) {
+                                $row = mysqli_fetch_assoc($sql3);
+                                $_SESSION['unique_id'] = $row['unique_id'];
+                                echo "Success";
+                            } else {
+                                echo "This email address not exist";
+                            }
+                        } else {
+                            echo "Something went wrong";
+                        }
+                    }
+                } else {
+                    echo "Please select an image file - jpg, png, jpeg";
+                }
+            } else {
+                echo "Please select an image file";
+            }
+        }
+    } else {
+        echo "$email - This is not a valid email";
+    }
+} else {
+    echo 'All input field are required!';
+}
